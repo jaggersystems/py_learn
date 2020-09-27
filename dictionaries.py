5045a8f3fb8d3906938b5a5b2e1ccf1a1e11a8be
Stefan Jagger <stefan@jaggersystems.com>
2020-09-27 21:31:13 +0100

    calc, dictionaries, guess game, while loops

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "Feburary",
+    "Mar": "March",
+}
+
+# print(monthConversions["Jan"])
+
+print(monthConversions.get("Feb", "Not a valid key"))
---
+++ while_loop.py
@@ -0,0 +1,8 @@
+
+i = 7
+while i <= 10:
+    print(i)
+    i += 1
+    # i = i + 1   ## same as above shortcut
+
+print("Done looping")
